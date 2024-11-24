65317ebdf6ffa5a15458f52e5b21c5bf9b1e6464
wildanniam <wildanniam4@gmail.com>
2024-11-24 10:18:55 +0700

    POST API Stateful done

--- lib/pages/home_stateful.dart
+++ lib/pages/home_stateful.dart
@@ -1,3 +1,4 @@
+import 'package:api_flutter/models/http_stateful.dart';
 import 'package:flutter/material.dart';
 
 class HomeStateful extends StatefulWidget {
@@ -8,6 +9,7 @@ class HomeStateful extends StatefulWidget {
 }
 
 class _HomeStatefulState extends State<HomeStateful> {
+  HttpStateful dataResponse = HttpStateful();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,19 +22,23 @@ class _HomeStatefulState extends State<HomeStateful> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const FittedBox(
+            FittedBox(
               child: Text(
-                "ID : Belum ada data",
-                style: TextStyle(fontSize: 20),
+                dataResponse.id == null
+                    ? "ID : Belum ada data"
+                    : "ID: ${dataResponse.id}",
+                style: const TextStyle(fontSize: 20),
               ),
             ),
             const SizedBox(height: 20),
             const FittedBox(
                 child: Text("Name : ", style: TextStyle(fontSize: 20))),
-            const FittedBox(
+            FittedBox(
               child: Text(
-                "Belum ada data",
-                style: TextStyle(
+                dataResponse.name == null
+                    ? "Name : Belum ada data"
+                    : "${dataResponse.name}",
+                style: const TextStyle(
                   fontSize: 20,
                 ),
               ),
@@ -40,10 +46,12 @@ class _HomeStatefulState extends State<HomeStateful> {
             const SizedBox(height: 20),
             const FittedBox(
                 child: Text("Job : ", style: TextStyle(fontSize: 20))),
-            const FittedBox(
+            FittedBox(
               child: Text(
-                "Belum ada data",
-                style: TextStyle(
+                dataResponse.job == null
+                    ? "Job : Belum ada data"
+                    : "${dataResponse.job}",
+                style: const TextStyle(
                   fontSize: 20,
                 ),
               ),
@@ -51,17 +59,26 @@ class _HomeStatefulState extends State<HomeStateful> {
             const SizedBox(height: 20),
             const FittedBox(
                 child: Text("Created At : ", style: TextStyle(fontSize: 20))),
-            const FittedBox(
+            FittedBox(
               child: Text(
-                "Belum ada data",
-                style: TextStyle(
+                dataResponse.createdAt == null
+                    ? "Created At : Belum ada data"
+                    : "${dataResponse.createdAt}",
+                style: const TextStyle(
                   fontSize: 20,
                 ),
               ),
             ),
             const SizedBox(height: 100),
             OutlinedButton(
-              onPressed: () {},
+              onPressed: () {
+                HttpStateful.connectAPI("Wildan", "Flutter Dev").then((value) {
+                  print(value.name);
+                  setState(() {
+                    dataResponse = value;
+                  });
+                });
+              },
               child: const Text(
                 "POST DATA",
                 style: TextStyle(
